3ae92b5fd5a81eb583de5ece4599f0f0a3f7c4cc
kerzzq <htblearning@gmail.com>
2025-01-19 13:14:33 +0100

    Arreglo load_data

--- src/model/load_data.py
+++ src/model/load_data.py
@@ -150,63 +150,18 @@ cur.execute("""
 with open('../usuarios.json', 'r') as f:
 	usuarios = json.load(f)['usuarios']
 
-for user in usuarios:
-	dataBase_password = user['password'] + salt
-	hashed = hashlib.md5(dataBase_password.encode())
-	dataBase_password = hashed.hexdigest()
-	cur.execute(
-	    "INSERT INTO User VALUES (NULL, ?, ?, ?, ?, ?, ?)", 
-	    (user['nombres'], user['apellidos'], user['fecha_nac'], user['email'], dataBase_password, user['admin'])
-	)
+try:
 
-	con.commit()
+	for user in usuarios:
+		dataBase_password = user['password'] + salt
+		hashed = hashlib.md5(dataBase_password.encode())
+		dataBase_password = hashed.hexdigest()
+		cur.execute(
+		    "INSERT INTO User VALUES (NULL, ?, ?, ?, ?, ?, ?)", 
+		    (user['nombres'], user['apellidos'], user['fecha_nac'], user['email'], dataBase_password, user['admin'])
+		)
 
-### Insert books, copies, authors and themes
-with open('libros.tsv', 'r', encoding="UTF-8") as f:
-	libros = [x.split("\t") for x in f.readlines()]
-
-for author, title, cover, description, theme in libros:
-    try:
-        theme = theme.strip()
-        # Verificar si el autor ya existe
-        res = cur.execute("SELECT id FROM Author WHERE name=?", (author,))
-        author_row = res.fetchone()
-        if author_row is None:
-            # Si no existe, insertar el autor
-            cur.execute("INSERT INTO Author VALUES (NULL, ?)", (author,))
-            con.commit()
-            author_id = cur.lastrowid
-        else:
-            # Si existe, obtener su ID
-            author_id = author_row[0]
-
-        # Verificar si el tema ya existe
-        res = cur.execute("SELECT id FROM Theme WHERE name=?", (theme,))
-        theme_row = res.fetchone()
-        if theme_row is None:
-            # Si no existe, insertar el tema
-            cur.execute("INSERT INTO Theme VALUES (NULL, ?)", (theme,))
-            con.commit()
-            theme_id = cur.lastrowid
-        else:
-            # Si existe, obtener su ID
-            theme_id = theme_row[0]
-
-        # Insertar el libro
-        cur.execute("INSERT INTO Book VALUES (NULL, ?, ?, ?, ?)",
-					(title, author_id, cover, description.strip()))
-        con.commit()
-        book_id = cur.lastrowid
-
-		# Crear tres copias del libro
-        for i in range(3):
-            cur.execute("INSERT INTO Copy VALUES (NULL, ?)", (book_id,))
-            con.commit()
-
-		# Asociar el libro con el tema
-        cur.execute("INSERT INTO BookTheme VALUES (?, ?)", (book_id, theme_id))
-        con.commit()
-
-    except Exception as e:
-        print(f"Error al procesar el libro {title}: {str(e)}")
-        con.rollback()
+		con.commit()
+		
+except:
+	print("bd reiniciada")
